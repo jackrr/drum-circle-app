b2b552bec4c1d7a4acf53c7b37cc36e09f0aa9a9
vk <VKorelsky@users.noreply.github.com>
2025-04-08 13:08:39 -0500

    styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,4 +4,6 @@
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div class="h-100 ">
+	{@render children()}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -29,9 +29,23 @@
 	}
 </script>
 
-<h1>Drum Circle</h1>
-{#if connection}
-	<button onclick={createCircle}>Create new circle</button>
-	<input bind:value={circleId} />
-	<button onclick={joinCircle} disabled={circleId.length < 1}>Join circle {circleId}</button>
-{/if}
+<div class="flex flex-col items-center pt-7">
+	<h1>Drum Circle</h1>
+	<div class="flex flex-col pt-7">
+		{#if connection}
+			<button
+				class="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+				onclick={createCircle}>Create new circle</button
+			>
+
+			<div class="flex my-7">
+				<input class="border mr-8" bind:value={circleId} placeholder="Circle ID" />
+				<button
+					class="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+					onclick={joinCircle}
+					disabled={circleId.length < 1}>Join circle {circleId}</button
+				>
+			</div>
+		{/if}
+	</div>
+</div>
