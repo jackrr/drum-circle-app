47421a68fdc547a9ec3b37fa8b1834f65bfeea98
Jack Ratner <jackratner@gmail.com>
2025-04-30 17:03:41 -0400

    fix: some hacks for preventing ios zoom
    imperfect, but some progress.
    
    also has a failed attempt at filling the safe area with color

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,41 @@
 <script lang="ts">
 	import '../app.css';
+	import { onMount, onDestroy } from 'svelte';
+	import { browser } from '$app/environment';
 
 	let { children } = $props();
+
+	function preventPinchZoom(e: TouchEvent) {
+		// TODO: This doesn't work on iOS
+		if (e.scale !== 1) {
+			e.preventDefault();
+		}
+	}
+
+	let lastTouchEnd = $state(new Date().getTime());
+
+	function preventDblTapZoom(e: TouchEvent) {
+		var now = new Date().getTime();
+		if (now - lastTouchEnd <= 300) {
+			e.preventDefault();
+		}
+		lastTouchEnd = now;
+	}
+
+	onMount(() => {
+		document.addEventListener('touchmove', preventPinchZoom, { passive: false });
+		document.addEventListener('touchend', preventDblTapZoom, { passive: false });
+	});
+
+	onDestroy(() => {
+		if (!browser) return;
+
+		document.removeEventListener('touchmove', preventPinchZoom);
+		document.removeEventListener('touchend', preventDblTapZoom);
+	});
 </script>
 
-<div class="h-screen w-screen flex flex-col">
+<div class="flex h-screen w-screen flex-col bg-gr-200 p-safe">
 	<div class="flex-grow">
 		{@render children()}
 	</div>
